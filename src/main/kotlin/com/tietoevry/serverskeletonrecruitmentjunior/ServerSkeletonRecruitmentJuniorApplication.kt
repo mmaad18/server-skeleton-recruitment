9f61e13af5e86d25d7bd97e1704cd806bb9cc676
yahya <mohamed.yahya.maad@tietoevry.com>
2024-06-28 11:27:58 +0200

    First commit :D

---
+++ src/main/kotlin/com/tietoevry/serverskeletonrecruitmentjunior/ServerSkeletonRecruitmentJuniorApplication.kt
@@ -0,0 +1,11 @@
+package com.tietoevry.serverskeletonrecruitmentjunior
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerSkeletonRecruitmentJuniorApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerSkeletonRecruitmentJuniorApplication>(*args)
+}
---
+++ src/test/kotlin/com/tietoevry/serverskeletonrecruitmentjunior/ServerSkeletonRecruitmentJuniorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tietoevry.serverskeletonrecruitmentjunior
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerSkeletonRecruitmentJuniorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
